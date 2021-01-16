8831027931a834c73318541e02b9dcc795a1ce8d
truza <tiadoraruza@gmail.com>
2021-01-15 23:15:27 -0500

    Add empty activities

---
+++ app/src/main/java/com/example/coursecatchr/QueryingAPI.java
@@ -0,0 +1,14 @@
+package com.example.coursecatchr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QueryingAPI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_querying_a_p_i);
+    }
+}
